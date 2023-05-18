60e54e9e56cd573431a31f93f0f19a5cb1c5044f
danielatego <danielatego75@gmail.com>
2023-05-18 12:21:02 +0300

    using props destructured

--- src/app.js
+++ src/app.js
@@ -1,13 +1,37 @@
 import './style.css'
+import katie from "./components/images/katie.jpg";
+import bike from "./components/images/mountain-bike 1.jpg";
+import wedo from "./components/images/wedding-photography 1.jpg";
 import Nav from './components/nav'
 import Hero from './components/hero'
 import Card from './components/card'
 export default function App(){
+    
     return(
-    <app>
+    <>
         
-        <Nav/>
-        <Hero/>
-        <Card/>
-    </app>)
+        {/* <Nav/>
+        <Hero/>*/}
+        <Card
+        img ={katie} 
+        desc='Life lessons with Katie Zaferes'
+        price='From $136'
+        rating='5.0'
+        /> 
+        <Card
+        img ={bike} 
+        desc='Group Mountain Biking'
+        price='From $50'
+        rating='4.8'
+        />
+        <Card
+        img ={wedo} 
+        desc='Learn wedding photography'
+        price='From $125'
+        rating='5.0'
+        />
+        
+        
+
+    </>)
 }
--- src/components/card.js
+++ src/components/card.js
@@ -2,12 +2,13 @@ import main from './images/katie.jpg'
 import icon from './images/Star.svg'
 import dot from "./images/dot.svg";
 
-export default function Card(){
+export default function Card({img,desc,price,rating}){
+    console.log({img,desc,price,rating});
     return(
         <>
         <div className='card'>
             <div className='card_image'>
-                <img src={main} alt=""/>
+                <img src={img} alt=""/>
             </div>
             <div className='card_banner'>
                 <span>SOLD OUT</span> 
@@ -15,42 +16,20 @@ export default function Card(){
             <div className='card_footer'>
                 <div className='card_footer_1'>
                     <img className = "card_footer_img_1"src={icon} alt="" />
-                    <span className = "card_footer_span_1">5.0</span>
+                    <span className = "card_footer_span_1">{rating}</span>
                     <span className ='card_footer_span_2'>(6)</span>
                     <img className = "card_footer_img_2" src={dot} alt="" />
                     <span className ='card_footer_span_2'>U.S.A</span>
                 </div>
                 <div>
-                    <span className='card_footer_desc'>Life lessons with Katie Zaferes</span>
+                    <span className='card_footer_desc'>{desc}</span>
                 </div>
                 <div>
-                    <span className='light'><span className='bold'>From $136</span> / person</span>
+                    <span className='light'><span className='bold'>{price}</span> / person</span>
                 </div>
             </div>
         </div>
-        <div className='card'>
-            <div className='card_image'>
-                <img src={main} alt=""/>
-            </div>
-            <div className='card_banner'>
-                <span>SOLD OUT</span> 
-            </div>
-            <div className='card_footer'>
-                <div className='card_footer_1'>
-                    <img className = "card_footer_img_1"src={icon} alt="" />
-                    <span className = "card_footer_span_1">5.0</span>
-                    <span className ='card_footer_span_2'>(6)</span>
-                    <img className = "card_footer_img_2" src={dot} alt="" />
-                    <span className ='card_footer_span_2'>U.S.A</span>
-                </div>
-                <div>
-                    <span className='card_footer_desc'>Life lessons with Katie Zaferes</span>
-                </div>
-                <div>
-                    <span className='light'><span className='bold'>From $136</span> / person</span>
-                </div>
-            </div>
-        </div>
+
         </>
     )
 }
